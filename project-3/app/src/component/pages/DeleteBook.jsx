549a36f9dea3bf28967000c498863b7fda636367
sabaree-88 <besabareesh@gmail.com>
2024-07-07 14:11:16 +0530

    image upload spinner and design

--- project-3/app/src/component/pages/DeleteBook.jsx
+++ project-3/app/src/component/pages/DeleteBook.jsx
@@ -1,18 +1,23 @@
 import axios from "axios";
+import { useState } from "react";
 import { FaWindowClose } from "react-icons/fa";
 import { Link, useNavigate, useParams } from "react-router-dom";
+import Spinner from "../Spinner";
 
 const DeleteBook = () => {
   const { id } = useParams();
   const navigate = useNavigate();
-
+  const [loading, setLoading] = useState(false);
   const handleDelete = () => {
+    setLoading(true);
     axios
       .delete(`http://localhost:3000/book/${id}`)
       .then((res) => {
+        setLoading(false);
         navigate("/");
       })
       .catch((err) => {
+        setLoading(false);
         console.error("Error deleting the book:", err);
         alert("Failed to delete the book. Please try again.");
       });
@@ -20,25 +25,29 @@ const DeleteBook = () => {
 
   return (
     <div className="bg-gray-800 min-h-[100vh] w-full flex justify-center items-center">
-      <div className="w-6/12 bg-white p-5 rounded shadow-lg">
-        <div className="flex justify-end">
-          <Link to="/">
-            <FaWindowClose className="text-red-600 text-2xl" />
-          </Link>
-        </div>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <div className="w-6/12 bg-white p-5 rounded shadow-lg">
+          <div className="flex justify-end">
+            <Link to="/">
+              <FaWindowClose className="text-red-600 text-2xl" />
+            </Link>
+          </div>
 
-        <div className="mt-4">
-          <h3 className="text-center text-2xl">
-            Are you sure you want to delete this book!
-          </h3>
-          <button
-            className="bg-red-600 px-8 py-2 text-white w-full my-4"
-            onClick={handleDelete}
-          >
-            Delete book
-          </button>
+          <div className="mt-4">
+            <h3 className="text-center text-2xl">
+              Are you sure you want to delete this book!
+            </h3>
+            <button
+              className="bg-red-600 px-8 py-2 text-white w-full my-4"
+              onClick={handleDelete}
+            >
+              Delete book
+            </button>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
--- project-3/app/src/component/pages/Home.jsx
+++ project-3/app/src/component/pages/Home.jsx
@@ -3,14 +3,22 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { FaRegEye, FaRegPlusSquare, FaRegEdit } from "react-icons/fa";
 import { IoTrashBinSharp } from "react-icons/io5";
+import Spinner from "../Spinner";
 
 const Home = () => {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
+    setLoading(true);
     axios
       .get("http://localhost:3000/book")
-      .then((res) => setData(res.data))
-      .catch((err) => console.log(err));
+      .then((res) => {
+        setData(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }, []);
 
   return (
@@ -20,40 +28,50 @@ const Home = () => {
           <FaRegPlusSquare className="text-green-600 text-3xl" />
         </Link>
       </div>
-      <div className="grid grid-cols-3 gap-4">
-        {data.map((item, index) => {
-          return (
-            <div key={item._id} className="bg-white rounded-md p-3">
-              <div className="details border-b-2">
-                <div className="top flex justify-between pb-5">
-                  <h2>Book Name: {item.title}</h2>
-                  <p>Published Year: {item.year}</p>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <div className="grid grid-cols-3 gap-4">
+          {data.map((item, index) => {
+            return (
+              <div key={item._id} className="bg-white rounded-md p-3">
+                <div className="flex justify-center items-center border-b-2">
+                  <div className="img w-6/12">
+                    <img
+                      src={`http://localhost:3000/${item.imagePath}`}
+                      alt={item.title}
+                    />
+                  </div>
+                  <div className="details w-6/12">
+                    <div className="top flex justify-between flex-col gap-3 pb-5">
+                      <h2>Book Name: {item.title}</h2>
+                      <p>Published Year: {item.year}</p>
+                      <h3>Author: {item.author}</h3>
+                    </div>
+                  </div>
                 </div>
-                <div className="bottom pb-2">
-                  <h3>Author: {item.author}</h3>
+                <div className="actions flex justify-around p-3">
+                  <div className="views">
+                    <Link to={`/view/${item._id}`}>
+                      <FaRegEye className="text-2xl text-green-600" />
+                    </Link>
+                  </div>
+                  <div className="edit">
+                    <Link to={`/edit/${item._id}`}>
+                      <FaRegEdit className="text-2xl text-blue-600" />
+                    </Link>
+                  </div>
+                  <div className="delete">
+                    <Link to={`/delete/${item._id}`}>
+                      <IoTrashBinSharp className="text-2xl text-red-600" />
+                    </Link>
+                  </div>
                 </div>
               </div>
-              <div className="actions flex justify-around p-3">
-                <div className="views">
-                  <Link to={`/view/${item._id}`}>
-                    <FaRegEye className="text-2xl text-green-600" />
-                  </Link>
-                </div>
-                <div className="edit">
-                  <Link to={`/edit/${item._id}`}>
-                    <FaRegEdit className="text-2xl text-blue-600" />
-                  </Link>
-                </div>
-                <div className="delete">
-                  <Link to={`/delete/${item._id}`}>
-                    <IoTrashBinSharp className="text-2xl text-red-600" />
-                  </Link>
-                </div>
-              </div>
-            </div>
-          );
-        })}
-      </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 };

23255e282d03fe50ee96520ebc46307a1dee31e9
sabaree-88 <besabareesh@gmail.com>
2024-09-07 22:08:18 +0530

    changes

--- project-3/app/src/component/Admin/Message.jsx
+++ project-3/app/src/component/Admin/Message.jsx
@@ -44,7 +44,9 @@ const Message = () => {
           },
         }
       );
-      setMessages(response.data.inquiries);
+      setMessages((prevMessages) =>
+        prevMessages.filter((message) => message._id !== id)
+      );
     } catch (error) {
       setError("Failed to load messages");
     }
@@ -58,27 +60,31 @@ const Message = () => {
         <h2 className="text-2xl font-bold mb-4">Messages</h2>
 
         <ul className="space-y-4">
-          {messages.map((message) => (
-            <li
-              key={message._id}
-              className="p-4 border rounded bg-slate-500 shadow relative"
-            >
-              <button
-                className="absolute z-10 right-4 top-2"
-                onClick={() => handleRemove(message._id)}
+          {Array.isArray(messages) && messages.length > 0 ? (
+            messages.map((message) => (
+              <li
+                key={message._id}
+                className="p-4 border rounded bg-slate-500 shadow relative"
               >
-                <IoIosClose className="w-6 h-6 hover:text-white" />
-              </button>
-              <h3 className="font-bold">
-                {message.firstName} {message.lastName}
-              </h3>
-              <p className="text-sm text-gray-600">{message.email}</p>{" "}
-              <p>{message.message}</p>
-              <p className="text-xs text-gray-400">
-                {new Date(message.date).toLocaleString()}{" "}
-              </p>
-            </li>
-          ))}
+                <button
+                  className="absolute z-10 right-4 top-2"
+                  onClick={() => handleRemove(message._id)}
+                >
+                  <IoIosClose className="w-6 h-6 hover:text-white" />
+                </button>
+                <h3 className="font-bold">
+                  {message.firstName} {message.lastName}
+                </h3>
+                <p className="text-sm text-gray-600">{message.email}</p>{" "}
+                <p>{message.message}</p>
+                <p className="text-xs text-gray-400">
+                  {new Date(message.date).toLocaleString()}{" "}
+                </p>
+              </li>
+            ))
+          ) : (
+            <div>No messages available</div>
+          )}
         </ul>
       </div>
     </Layout>
